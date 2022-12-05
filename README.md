fef5c7b766e99b9f98314b3b7b62133d5227c7f8
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2022-12-05 12:48:47 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angularbjkcnjz38ssw-mpmk--4200
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-bjkcnj-z38ssw)
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+  ],
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
